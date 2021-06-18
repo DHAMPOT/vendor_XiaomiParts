537ec7790738025925b89aeabe748c7d3520e55d
D4rkKnight21 <yusuff8621@gmail.com>
2021-06-18 18:14:38 +0700

    make only for specific device

--- Android.mk
+++ Android.mk
@@ -1,5 +1,5 @@
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),$(filter $(TARGET_DEVICE),lavender,platina))
+ifeq ($(TARGET_DEVICE),$(filter $(TARGET_DEVICE),platina))
 include $(call all-makefiles-under,$(LOCAL_PATH))
 endif
